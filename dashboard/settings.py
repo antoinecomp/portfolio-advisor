82cdbddc9d5f6a79dc04ac621b1e2c662bd15434
sigamani <michael.sigamani@gmail.com>
2020-02-10 10:45:58 +0000

    moving order of list

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -10,9 +10,9 @@ segment1 = {"label": "Undecided", "value": "UND"}
 segment2 = {"label": "Abstainer", "value": "ABS"}
 segment3 = {"label": "PJD", "value": "PJD"}
 segment4 = {"label": "PAM", "value": "PAM"}
-segment5 = {"label": "OTH", "value": "OTH"}
-segment6 = {"label": "RNI", "value": "RNI"}
-segment7 = {"label": "IST", "value": "IST"}
+segment5 = {"label": "RNI", "value": "RNI"}
+segment6 = {"label": "Istiqlal", "value": "IST"}
+segment7 = {"label": "Other", "value": "OTH"}
 SEGMENT_LIST = [segment1, segment2, segment3, segment4, segment5, segment6, segment7]
 
 DEFAULT_CLUSTERS = 'cluster_Forgotten|cluster_Aspirational Youth|cluster_Snowflakes|cluster_Average|cluster_PJD|cluster_OTH|cluster_PAM|cluster_Empty Nest Mothers|cluster_IST|cluster_RNI|cluster_Urban Professional'
