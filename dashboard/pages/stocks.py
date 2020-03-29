70c216f8e29b29e5a195ec022e2ce832516bac10
antoineccc <antoine.coppin@auspex.ai>
2020-03-29 17:39:25 +0100

    added treemap of all stocks according to country

--- dashboard/pages/stocks.py
+++ dashboard/pages/stocks.py
@@ -16,12 +16,14 @@ import numpy as np
 from ..server import app
 
 base_dir = dirname(dirname(abspath(__file__)))
-data_path = 'data/tickers_september_2017'
+data_path = 'data/tickers_september_2017.xlsx'
 
 
 def get_tickers():
-    with open(join(base_dir, data_path), 'r') as fp:
-        stock_df = pd.read_csv(fp)
+    with open(join(base_dir, data_path), 'rb') as fp:
+        stock_df = pd.read_excel(fp)
+        stock_df = stock_df.dropna()
+        # stock_df = stock_df.where(pd.notnull(stock_df), None)
         return stock_df
 
 
@@ -34,9 +36,9 @@ def layout():
             html.Div([
                 html.H5('Stocks', style={'textAlign': 'center', 'padding': 10}),
                 dcc.Graph(id='graph',
-                   figure=px.treemap(df, path=['Country', 'Exchange', 'ticker'], # values='pop',
+                   figure=px.treemap(df, path=['Country', 'Exchange'], # values='pop',
                   # color='lifeExp', hover_data=['iso_alpha'],
-                  color_continuous_scale='RdBu',
+                  # color_continuous_scale='RdBu',
                   # color_continuous_midpoint=np.average(df['lifeExp'], weights=df['pop'])
                 ))
             ], className='pretty_container twelve columns'),
